05156e3d1f96dce42a72f73d56d863b227ce947e
jusadocode <77744027+jusadocode@users.noreply.github.com>
2024-09-25 00:05:17 +0300

    Valuables, medicals, bullet speed, window size (#3)

--- RAID2D/Bullet.cs
+++ RAID2D/Bullet.cs
@@ -14,9 +14,16 @@ namespace RAID2D
         public int bulletLeft;
         public int bulletTop;
 
-        private int speed = 20;
+        private int speed = 30;
         private PictureBox bullet = new PictureBox();
         private Timer bulletTimer = new Timer();
+
+
+        int formWidth = 100; // Values upadate at makeBullet
+        int formHeight = 100;
+
+        private DateTime lastUpdateTime = DateTime.Now;
+
 
 
         public void MakeBullet(Form form)
@@ -31,6 +38,8 @@ namespace RAID2D
 
             form.Controls.Add(bullet);
 
+            formWidth = form.ClientSize.Width;
+            formHeight = form.ClientSize.Height;
 
             bulletTimer.Interval = speed;
             bulletTimer.Tick += new EventHandler(BulletTimerEvent);
@@ -41,28 +50,35 @@ namespace RAID2D
         private void BulletTimerEvent(object sender, EventArgs e)
         {
 
+            DateTime now = DateTime.Now;
+            double deltaTime = (now - lastUpdateTime).TotalSeconds; 
+            lastUpdateTime = now;
+
+            int deltaX = (int)(speed * deltaTime * 100);  // 100 is an arbitrary factor for scaling speed
+
+
             if (direction == "left")
             {
-                bullet.Left -= speed;
+                bullet.Left -= deltaX;
             }
 
             if (direction == "right")
             {
-                bullet.Left += speed;
+                bullet.Left += deltaX;
             }
 
             if (direction == "up")
             {
-                bullet.Top -= speed;
+                bullet.Top -= deltaX;
             }
 
             if (direction == "down")
             {
-                bullet.Top += speed;
+                bullet.Top += deltaX;
             }
 
 
-            if (bullet.Left < 10 || bullet.Left > 860 || bullet.Top < 10 || bullet.Top > 600)
+            if (bullet.Left < 10 || bullet.Left > formWidth - 10 || bullet.Top < 10 || bullet.Top > formHeight - 10)
             {
                 bulletTimer.Stop();
                 bulletTimer.Dispose();
---
+++ RAID2D/ValuableItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAID2D.Resources
+{
+    internal class ValuableItem
+    {
+        public string name { get; set; }
+        public int value { get; set; }
+        public Image image { get; set; }
+        public int spawnChance { get; set; }
+        public ValuableItem(string name, int value, int spawnChance, Image image) { 
+            this.name = name;
+            this.value = value;
+            this.image = image;
+            this.spawnChance = spawnChance;
+        }
+    }
+}
