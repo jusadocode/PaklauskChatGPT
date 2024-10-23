87e49b2ee58b08f1b30f21c2d5cefa4203387603
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-10-24 00:51:48 +0300

    nebezinau jau

--- Client/Properties/Resources.Designer.cs
+++ Client/Properties/Resources.Designer.cs
@@ -149,16 +149,7 @@ namespace Client.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] hitmarker {
-            get {
-                object obj = ResourceManager.GetObject("hitmarker", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
+       
         
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
