b8372f63ac2783d8d5359227c149f0896ead98cc
Aurimas <92021135+AurisTFG@users.noreply.github.com>
2024-10-24 04:56:25 +0300

    Add Rand.cs (static random number class)

---
+++ Client/Rand.cs
@@ -0,0 +1,16 @@
+﻿namespace Client;
+
+public static class Rand
+{
+    private static readonly Random _random = new();
+
+    public static int Next(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
+
+    public static double NextDouble()
+    {
+        return _random.NextDouble();
+    }
+}
