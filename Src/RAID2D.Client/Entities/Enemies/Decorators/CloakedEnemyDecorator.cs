03f6c1ab97f32c22e358ffb37b1ee39e796dfff9
jusadocode <77744027+jusadocode@users.noreply.github.com>
2024-11-13 23:35:36 +0200

    Another decorator (#23)

---
+++ Src/RAID2D.Client/Entities/Enemies/Decorators/CloakedEnemyDecorator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace RAID2D.Client.Entities.Enemies.Decorators;
+
+public class CloakedEnemyDecorator : EnemyDecorator
+{
+    private Timer cloakTimer;
+    private Timer visibilityTimer;
+    private Timer implosionTimer; 
+    private bool isCloaked;
+    private int cloakDuration = 2000;
+    private int visibilityDuration = 3000; 
+    private int implosionDuration = 1000; 
+    private PictureBox enemyPictureBox;
+    private Size originalSize; 
+    private int implosionStep = 10; 
+
+    public CloakedEnemyDecorator(IEnemy baseEnemy) : base(baseEnemy)
+    {
+        enemyPictureBox = baseEnemy.PictureBox;
+        originalSize = enemyPictureBox.Size; 
+        InitializeCloakingBehavior();
+    }
+
+    private void InitializeCloakingBehavior()
+    {
+        cloakTimer = new Timer { Interval = visibilityDuration };
+        visibilityTimer = new Timer { Interval = cloakDuration };
+        implosionTimer = new Timer { Interval = implosionDuration / (originalSize.Width / implosionStep) }; 
+
+        cloakTimer.Tick += (s, e) => StartImplosion();
+        visibilityTimer.Tick += (s, e) => Uncloak();
+        implosionTimer.Tick += (s, e) => Implode();
+
+        cloakTimer.Start(); 
+    }
+
+    private void StartImplosion()
+    {
+        if (isCloaked)
+            return;
+
+        isCloaked = true;
+        implosionTimer.Start(); 
+        cloakTimer.Stop();
+    }
+
+    private void Implode()
+    {
+        if (enemyPictureBox.Width > implosionStep && enemyPictureBox.Height > implosionStep)
+        {
+            enemyPictureBox.Width -= implosionStep;
+            enemyPictureBox.Height -= implosionStep;
+
+            enemyPictureBox.Left += implosionStep / 2;
+            enemyPictureBox.Top += implosionStep / 2;
+        }
+        else
+        {
+            enemyPictureBox.Visible = false;
+            implosionTimer.Stop();
+            visibilityTimer.Start(); 
+        }
+    }
+
+    private void Uncloak()
+    {
+        if (!isCloaked)
+            return;
+
+        isCloaked = false;
+        enemyPictureBox.Visible = true; 
+        enemyPictureBox.Size = originalSize; 
+        enemyPictureBox.Left -= (originalSize.Width - enemyPictureBox.Width) / 2; 
+        enemyPictureBox.Top -= (originalSize.Height - enemyPictureBox.Height) / 2; 
+        cloakTimer.Start();
+        visibilityTimer.Stop();
+    }
+
+    public override void UpdateAppearance()
+    {
+        if (isCloaked)
+        {
+            enemyPictureBox.BackColor = Color.Transparent;
+        }
+        else
+        {
+            enemyPictureBox.BackColor = Color.Red;
+        }
+    }
+}
+
