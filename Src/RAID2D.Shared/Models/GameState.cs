cfe3f0ea35dab66ee1b0e4e45e77e7f9c7fd88c9
Justas Adomaitis <justas.ado@gmail.com>
2024-12-19 04:26:16 +0200

    visitor

--- Src/RAID2D.Shared/Models/GameState.cs
+++ Src/RAID2D.Shared/Models/GameState.cs
@@ -8,15 +8,21 @@ public class GameState
     public string ConnectionID { get; set; } = "";
     public Point Location { get; set; }
     public Direction Direction { get; set; }
+    public bool IsDead { get; set; }
+    public uint Kills { get; set; }
+    public uint Cash { get; set; }
 
-    public GameState(Point location, Direction direction)
+    public GameState(Point location, Direction direction, bool isDead, uint kills, uint cash)
     {
         this.Location = location;
         this.Direction = direction;
+        this.IsDead = isDead;
+        this.Kills = kills;
+        this.Cash = cash;
     }
 
     public override string ToString()
     {
-        return $"ConnectionID={ConnectionID}, Location={Location}, Direction={Direction}";
+        return $"ConnectionID={ConnectionID}, Location={Location}, Direction={Direction} isDead={IsDead}, Kills={Kills}, Cash={Cash}";
     }
 }
